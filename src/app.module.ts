c8d3ee7cfd870554059c3c86f1fc0af72bf8fd33
Your Name <you@example.com>
2025-06-18 19:53:30 +0500

    feat: базовая настройка проекта и инфраструктуры
    - Подключён ConfigModule
    - Настроена .env структура
    - Добавлен ValidationPipe
    - Настроен CORS
    - Установлен Swagger
    - Добавлен docker-compose.yml с PostgreSQL
    - Установлен Prisma, добавлен schema.prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { IS_DEV_ENV } from './common/utils/is-dev.utils';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the configuration available globally
+      ignoreEnvFile: !IS_DEV_ENV, // Ignores .env file in production
+      envFilePath: IS_DEV_ENV ? '.env' : '.env.production', // Loads different .env files based on the environment
+    }),
+    PrismaModule,
+  ],
 })
 export class AppModule {}
